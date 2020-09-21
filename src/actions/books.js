3794d71484655fdbae86a8b14a79d0d3c2c854f6
Fernando <fernando.bombassaro@gmail.com>
2020-09-21 01:17:25 -0300

    Adicionado form para new book

--- src/actions/books.js
+++ src/actions/books.js
@@ -3,11 +3,14 @@ import axios from 'axios'
 const api_root = `http://localhost:3001/api`
 
 const books_list = () => {
-
   return axios.post(`${api_root}/books/list`)
     .then(payload => payload.data.items)
     .catch(e => console.error(e))
-
+}
+const books_create = (payload) => {
+  return axios.post(`${api_root}/books/add`, payload )
+    .then(payload => payload.data.items)
+    .catch(e => console.error(e))
 }
 
-export {books_list}
+export {books_list, books_create}
--- src/components/PageCreate.js
+++ src/components/PageCreate.js
@@ -1,18 +1,23 @@
 import React from 'react';
 import {map} from 'lodash';
 
+import {Select, MenuItem} from '@material-ui/core';
+
 import BookCard from './BookCard';
 
-import {books_list} from '../actions/books'
+import {books_create, books_list} from '../actions/books'
 import {doctors_list} from '../actions/doctors'
 import {patients_list} from '../actions/patients'
 import {skills_list} from '../actions/skills'
 
 const PageCreate = () => {
   
+  const [selected_doctor, select_doctor] = React.useState("");
+  const [selected_patient, select_patient] = React.useState("");
+
   const [items, set_items] = React.useState([]);
   const [doctors, set_doctors] = React.useState([]);
-  const [patient, set_patient] = React.useState([]);
+  const [patients, set_patient] = React.useState([]);
   const [skills, set_skills] = React.useState([]);
 
   React.useEffect(() => {
@@ -21,6 +26,14 @@ const PageCreate = () => {
     patients_list().then(payload => set_patient(payload))
     skills_list().then(payload => set_skills(payload))
   }, [])
+  
+  const selectDoctor = ({target}) => select_doctor(target.value)
+  const selectPatient = ({target}) => select_patient(target.value)
+
+  const create_new_book = () => {
+    let object = {"name": selected_doctor, "patient": selected_patient, "date": new Date(), status: 'Open'}
+    books_create(object)
+  }
 
   if(!items)
     return <pre>no-items</pre>
@@ -31,13 +44,25 @@ const PageCreate = () => {
       <pre>Consultas agendadas {items.length}</pre>
       
       <pre>Doctors {doctors.length}</pre>
-      <pre>Patients {patient.length}</pre>
+      <pre>Patients {patients.length}</pre>
       <pre>Skills {skills.length}</pre>
       
       {map(items, (item, key) => 
         <BookCard key={key} content={item} onClick={() => console.log(`click ${key}`)} />)}
 
-      <pre>Agendar nova consulta</pre>
+      <Select onChange={selectDoctor} label="Doctor">
+        <MenuItem value={selected_doctor}>Select Doctor</MenuItem>
+        {map(doctors, (item, key) => 
+          <MenuItem key={key} value={item.name}>{item.name}</MenuItem>)}
+      </Select>
+
+      <Select onChange={selectPatient} label="Patient">
+        <MenuItem value={selected_patient}>Select Patient</MenuItem>
+        {map(patients, (item, key) => 
+          <MenuItem key={key} value={item.name}>{item.name}</MenuItem>)}
+      </Select>
+
+      <pre onClick={() => create_new_book()}>Agendar nova consulta</pre>
     </div>
   );
 };
